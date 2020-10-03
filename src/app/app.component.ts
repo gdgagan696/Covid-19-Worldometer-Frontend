9e5e7e43e6ece4c511f1b57df43234eb5844dc5a
gdgagan696 <gagan.dhand@non.se.com>
2020-10-03 15:42:02 +0530

    change in UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { InfoDialogComponent } from './dialog/info-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid19-fe';
+
+  constructor(private dialog:MatDialog){}
+
+openAboutUs(){
+  const dialogConfig = new MatDialogConfig();
+  dialogConfig.width='500px',
+  dialogConfig.height='300px',
+  dialogConfig.autoFocus = true;
+  this.dialog.open(InfoDialogComponent,dialogConfig);
+}
 }
---
+++ src/app/dialog/info-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.css']
+})
+export class InfoDialogComponent implements OnInit {
+
+  constructor(private dialogRef: MatDialogRef<InfoDialogComponent>) { }
+
+  ngOnInit() {
+  }
+
+  closeDialog(){
+    this.dialogRef.close();
+  }
+}
